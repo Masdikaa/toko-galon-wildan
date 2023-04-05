857458cbc14c8731b7e9251410389fffd768ae48
Masdikaa <masdikailhanmansiz@gmail.com>
2023-04-05 19:25:36 +0700

    first commit

---
+++ app/src/main/java/com/masdika/tokogalonkematian/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.masdika.tokogalonkematian
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.masdika.tokogalonkematian.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Alwas Light Mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iv.alpha = 0f
+        binding.iv.animate().setDuration(1500).alpha(1f).withEndAction {
+            startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+    }
+}
